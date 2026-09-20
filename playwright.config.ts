9093145863db1543b4735470046a7a397327fb96
Joaquim Cartaxo <jcartaxo.bjj@gmail.com>
2026-09-20 01:17:05 -0300

    Corrige versão node e adiciona trace nos testes

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,8 @@ export default defineConfig({
   reporter: "html",
   use: {
     baseURL: "http://127.0.0.1:5173",
-    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    trace: "retain-on-failure",
   },
   projects: [
     { name: "chromium", use: { ...devices["Desktop Chrome"] } },
